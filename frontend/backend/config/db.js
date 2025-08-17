5642c7c3f0d105d6e8277f4b3ab40861abc78cb8
arvind6206 <vishwkarmaarvind1222@gmail.com>
2025-08-17 13:18:43 +0530

    delete duplicate file

--- frontend/backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from 'mongoose'
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://vishwkarmaarvind1222:arvind815317@completecoding.mdabdwf.mongodb.net/food-del').then(()=>console.log("DB Connected"))
-}
